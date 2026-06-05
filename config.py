acab055f4af85625a4525a9bf09f411f7d2e75ab
stebil87 <97510620+stebil87@users.noreply.github.com>
2026-06-05 20:59:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 AZIENDA = {
-    "ragione_sociale": "1908 Group SA",
-    "indirizzo_1": "Via Cantonale 18",
-    "indirizzo_2": "Suglio Business Center",
-    "npa_localita": "6928 Manno",
-    "numero_idi": "CHE-296.614.693",
-    "comune_fiscale": "6928 Manno",
-    "numero_controllo": "100.78.021.890",
+    "ragione_sociale": "Nome Azienda SA",
+    "indirizzo_1":     "Via Example 1",
+    "indirizzo_2":     "",
+    "npa_localita":    "0000 Citta",
+    "numero_idi":      "CHE-XXX.XXX.XXX",
+    "comune_fiscale":  "0000 Citta",
+    "numero_controllo":"XXX.XX.XXX.XXX",
 }
 
 MANAGER_USERNAME = st.secrets["MANAGER_USERNAME"]
